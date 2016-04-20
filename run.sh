cd37fb97783c9f6a0c6b28660b518a9aac7070e9
Bram Staps <github@bramstaps.nl>
2016-04-20 22:37:12 +0200

    crnl to confirm more to http standards

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 #You should have received a copy of the GNU Affero General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-socat TCP-LISTEN:80,reuseaddr,fork EXEC:./main.py >/var/log/httpwn.org.log
+socat TCP-LISTEN:80,reuseaddr,fork,crnl EXEC:./main.py >/var/log/httpwn.org.log
